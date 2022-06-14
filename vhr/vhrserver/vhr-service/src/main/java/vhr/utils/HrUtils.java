5d47396f9c4d56b1080853a8380f112f01015436
gaodianxiaoye78 <1223491134@qq.com>
2022-06-14 11:06:07 +0800

    第一次项目搭建

---
+++ vhr/vhrserver/vhr-service/src/main/java/vhr/utils/HrUtils.java
@@ -0,0 +1,10 @@
+package vhr.utils;
+
+import cn.gdut.vhr.model.Hr;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+public class HrUtils {
+    public static Hr getCurrentHr() {
+        return ((Hr) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+    }
+}
---
+++ vhr/vhrserver/vhr-web/src/main/java/cn/gdut/vhr/VhrApplication.java
@@ -0,0 +1,17 @@
+package cn.gdut.vhr;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableCaching
+@MapperScan(basePackages = "cn.gdut.vhr.mapper")
+@EnableScheduling
+public class VhrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VhrApplication.class, args);
+    }
+}
